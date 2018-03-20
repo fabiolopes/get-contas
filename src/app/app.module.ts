fff1475ea8ee77170d1a49aedaf148d631129590
fabio.bione <fabio.bione@accenture.com>
2018-03-20 17:12:18 -0300

    Adição modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
+import { ContaService } from './conta.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpModule} from '@angular/http';
 
 
 import { AppComponent } from './app.component';
+import { NovaContaComponent } from './nova-conta/nova-conta.component';
+import { Configuration } from './ap.constants';
+import { SimNaoPipe } from './sim-nao.pipe';
+import { NovoItemComponent } from './novo-item/novo-item.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovaContaComponent,
+    SimNaoPipe,
+    NovoItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [ContaService, Configuration],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
